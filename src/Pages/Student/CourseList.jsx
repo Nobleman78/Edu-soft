256e34f9a1eb9419d59ed4eac5476ea1e491ad9b
Jesmin Chakma <jesminchakma39@gmail.com>
2025-03-24 17:50:35 +0600

    Course Filter Added

--- src/Pages/Student/CourseList.jsx
+++ src/Pages/Student/CourseList.jsx
@@ -1,21 +1,49 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+
 import { ContextApi } from '../../Context/Context';
 import CourseCard from '../../components/Student/CourseCard';
+import { CiSearch } from "react-icons/ci";
+import { FiFilter } from 'react-icons/fi';
+import Searchbar from '../../components/Student/Searchbar';
+import { Link, useParams } from 'react-router-dom';
 
 const CourseList = () => {
     const { courses } = useContext(ContextApi)
+    const {input } = useParams();
+   
+
+    // Course Filter
+    const [filterCourse,setFilterCourse] = useState([]);
+    console.log(filterCourse)
+
+    useEffect(()=>{
+        if(courses.length > 0){
+            const temCourse = courses.slice();
+            input ?setFilterCourse(
+                temCourse.filter(item=>item.courseTitle.toLowerCase().includes(input.toLowerCase()))
+            )  :setFilterCourse(temCourse)
+        }
+
+    },[courses,input])
+
+
+
     return (
-        <div className='mt-10 md:mt-12'>
-            <h2 className='text-4xl font-semibold'>Course list</h2>
-            <p><Link to='/'><span className='text-blue-600'>Home</span> </Link>/<span className='text-gray-500'> Course list</span></p>
+        <div className='mt-20 md:mt-20 '>
+            <div className='flex flex-col md:flex-row items-center gap-2  justify-between px-8 '>
+                <div className='flex flex-col gap-2'>
+                    <h2 className='text-4xl font-semibold'>Course list</h2>
+                    <p className='text-center md:text-start'><Link to='/'><span className='text-blue-600'>Home</span> </Link>/<span className='text-gray-500'> Course list</span></p>
+                </div>
+                <Searchbar data={input}/>
+
+
+            </div>
             <div className='mt-10 py-10 lg:px-8  '>
-                <h2 className='text-center text-3xl font-medium'>Our Courses</h2>
-                <p className='text-center mb-3 mt-3 '>Discover our top-rated courses across various categories. From coding and design to business and wellness, our courses are crafted to deliver results.</p>
                 <div>
                     {
                         <div className='grid grid-cols-1  md:grid-cols-2 lg:grid-cols-4 px-4 md:px-0 md:my-16 my-10 gap-4'>
-                            {courses.map((item, index) => <CourseCard key={index} course={item} />)}
+                            {filterCourse.map((item, index) => <CourseCard key={index} course={item} />)}
                         </div>
                     }
                 </div>
--- src/components/Student/Searchbar.jsx
+++ src/components/Student/Searchbar.jsx
@@ -8,9 +8,10 @@ const Searchbar = ({data}) => {
     const onSearchHandler = (e) => {
         e.preventDefault();
         navigate('/course-list/' + input)
+        setInput('')
     }
     return (
-        <form onSubmit={onSearchHandler} className='flex items-center justify-between border py-3  border-gray-500/20 rounded w-80 mt-3 md:w-[50%] mx-auto relative bg-white '>
+        <form onSubmit={onSearchHandler} className='flex items-center justify-between border py-3  border-gray-500/20 rounded w-70 mt-3 md:w-[50%] mx-auto relative bg-white '>
             <div className='flex items-center'>
                 <CiSearch className='md:w-auto w-10 px-3 text-xl' />
                 <input onChange={(e)=>setInput(e.target.value)} value={input} className=' outline-none shadow-none w-100' type="text" placeholder='Search here' />
